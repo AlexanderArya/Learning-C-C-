882f7e1ada6cd7a80d9cd35fada56b2fe9c211a6
Andrean Bimo <bimowibowo4846@gmail.com>
2024-07-30 15:13:59 +0700

    Program Fungsi Dasar

---
+++ FungsiDasar/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Nama : Andrean Vini Bimo Arya
+//NIM  : 672021256
+
+void Bintang2(){
+printf(" ");printf(" ");printf("*");printf("*");printf(" ");printf(" ");printf("\n");
+}
+
+void Bintang4(){
+printf(" ");printf("*");printf("*");printf("*");printf("*");printf(" ");printf("\n");
+}
+
+void Bintang6(){
+printf("*");printf("*");printf("*");printf("*");printf("*");printf("*");printf("\n");
+}
+
+
+void main()
+{
+  Bintang2();
+  Bintang4();
+  Bintang6();
+  Bintang6();
+  Bintang4();
+  Bintang2();
+}
