a268a0af4330dd97a3342b0c3ab74cc39340ba70
Andrean Bimo <bimowibowo4846@gmail.com>
2024-07-30 14:58:47 +0700

    Printf Bintang C

---
+++ PrintfBintang/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bintang(){
+printf(" Hello World ");
+}
+
+void main()
+{
+    bintang();
+    printf("KOTAK PERTAMA: \n");
+    printf("****\n");
+    printf("****\n");
+    printf("****\n\n\n");
+
+    printf("Segitiga :\n");
+    printf("*");printf("\n");
+    printf("*");printf("*");printf("\n");
+    printf("*");printf("*");printf("*");printf("\n\n\n\n");
+
+    printf("Segitiga Kebalik : \n");
+    printf(" ");printf(" ");printf("*");printf("\n");
+    printf(" ");printf("*");printf("*");printf("\n");
+    printf("*");printf("*");printf("*");printf("\n");
+
+    printf("Pembuatan Kotak Double : \n");
+    printf("*");printf("*");printf("*");printf("*");printf("*");printf("*");printf("*");printf("*");printf("\n");
+    printf("*");printf(" ");printf(" ");printf("*");printf("*");printf(" ");printf(" ");printf("*");printf("\n");
+    printf("*");printf(" ");printf(" ");printf("*");printf("*");printf(" ");printf(" ");printf("*");printf("\n");
+    printf("*");printf(" ");printf(" ");printf("*");printf("*");printf(" ");printf(" ");printf("*");printf("\n");
+    printf("*");printf(" ");printf(" ");printf("*");printf("*");printf(" ");printf(" ");printf("*");printf("\n");
+    printf("*");printf("*");printf("*");printf("*");printf("*");printf("*");printf("*");printf("*");printf("\n\n\n\n");
+
+    printf("* ");printf("* ");printf("* ");printf("* ");printf("* ");printf("* ");printf("*\n");
+    printf("* ");printf("  ");printf("  ");printf("  ");printf("  ");printf("  ");printf("*\n");
+    printf("* ");printf("  ");printf("  ");printf("  ");printf("  ");printf("  ");printf("*\n");
+    printf("* ");printf("  ");printf("  ");printf("  ");printf("  ");printf("  ");printf("*\n");
+    printf("* ");printf("* ");printf("* ");printf("* ");printf("* ");printf("* ");printf("*\n");
+
+
+
+}
